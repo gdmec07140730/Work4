e95af3d8bc2937c50d00d2ec824d731573310885
Serene <310253770@qq.com>
2015-12-28 19:40:31 +0800

    git two

--- app/src/main/java/com/example/administrator/mycontacts/AddContactsActivity.java
+++ app/src/main/java/com/example/administrator/mycontacts/AddContactsActivity.java
@@ -18,6 +18,7 @@ public class AddContactsActivity extends Activity {
     private EditText addressET;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
